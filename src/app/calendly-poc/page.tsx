42e1df3a6085ec887630141e80c0b23d92de2c4f
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-07-12 09:26:29 +0530

    Added sample calendly implementation

---
+++ src/app/calendly-poc/page.tsx
@@ -0,0 +1,7 @@
+import BookAppointment from "@/components/book-appointment";
+
+export default function CalendlyPOCPage() {
+    return <div className=" h-screen flex justify-center items-center w-full">
+        <BookAppointment/>
+    </div>
+}
