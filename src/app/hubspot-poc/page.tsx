cde9621a57bcff1fcaa2b05c867f6be82e814c49
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-07-12 09:41:38 +0530

    Added HubSpot sample

---
+++ src/app/hubspot-poc/page.tsx
@@ -0,0 +1,7 @@
+import { HubspotContactForm } from "@/components/hubspot-from";
+
+export default function HubSpotPOCPage() {
+    return <div className="">
+        <HubspotContactForm />
+    </div>   
+}
