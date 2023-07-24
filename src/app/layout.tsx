e6169632c95c9ad95006b0463e3960d06dcd11a8
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-07-24 13:09:42 +0530

    Added sticky header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,11 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
 import TwitterIcon from '@/icons/twitter.icon';
 import LinkedInIcon from '@/icons/linkedin.icon';
 import YoutubeIcon from '@/icons/youtube.icon';
-import ThemeToggleAndHamburger from '@/components/theme-toggle-and-hamburger';
+import Header from '@/components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,74 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header>
-          <nav>
-            <div
-              className="
-                flex
-                justify-between
-                px-6
-                py-3
-                relative
-                xl:px-20
-                xl:pt-3
-                xl:pb-4
-              "
-            >
-              <div>
-                <Image
-                  className=" lg:w-[173px] lg:h-[52px]"
-                  width={120}
-                  height={36}
-                  alt="Root On logo"
-                  src={'/r-oot-on-logo-svg.svg'}
-                />
-              </div>
-              <div
-                className="
-                  gap-[62px]
-                  justify-end
-                  items-center
-                  text-white
-                  text-base
-                  font-bold
-                  hidden
-                  lg:flex
-                "
-              >
-                <span>
-                  <Link href={'/'}> About Us </Link>
-                </span>
-                <span>
-                  <Link href={'/'}> Services </Link>
-                </span>
-                <span>
-                  <Link href={'/'}> Coaching </Link>
-                </span>
-                <span>
-                  <Link href={'/'}> Blogs </Link>
-                </span>
-                <span>
-                  <Link href={'/'}> Contact Us </Link>
-                </span>
-                <span>
-                  <Link href={'/'}> Tools </Link>
-                </span>
-              </div>
-              <ThemeToggleAndHamburger />
-            </div>
-            <div
-              className="
-                  border-b
-                  border-[#d8d8d8]
-                  h-0
-                  mx-6
-                  opacity-50
-                  xl:mx-20
-                "
-            />
-          </nav>
-        </header>
+        <Header />
         {children}
         <footer
           className="
---
+++ src/components/header.tsx
@@ -0,0 +1,113 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import ThemeToggleAndHamburger from './theme-toggle-and-hamburger';
+
+export default function Header() {
+  const [srolledEnough, setSrolledEnough] = useState(false);
+
+  useEffect(() => {
+    let lastKnownScrollPosition = 0;
+    let ticking = false;
+
+    function showOrHideHeader(scrollPos: number) {
+      if (scrollPos > 80) {
+        setSrolledEnough(true);
+      } else {
+        setSrolledEnough(false);
+      }
+    }
+
+    function onScroll() {
+      lastKnownScrollPosition = window.scrollY;
+
+      if (!ticking) {
+        window.requestAnimationFrame(() => {
+          showOrHideHeader(lastKnownScrollPosition);
+          ticking = false;
+        });
+
+        ticking = true;
+      }
+    }
+
+    document.addEventListener('scroll', onScroll);
+
+    return () => {
+      document.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
+  return (
+    <header className={`${ srolledEnough ? ' fixed w-full' : ' relative text' }`}>
+      <nav>
+        <div
+          className="
+          flex
+          justify-between
+          px-6
+          py-3
+          relative
+          xl:px-20
+          xl:pt-3
+          xl:pb-4
+        "
+        >
+          <div>
+            <Image
+              className=" lg:w-[173px] lg:h-[52px]"
+              width={120}
+              height={36}
+              alt="Root On logo"
+              src={'/r-oot-on-logo-svg.svg'}
+            />
+          </div>
+          <div
+            className="
+            gap-[62px]
+            justify-end
+            items-center
+            text-white
+            text-base
+            font-bold
+            hidden
+            lg:flex
+          "
+          >
+            <span>
+              <Link href={'/'}> About Us </Link>
+            </span>
+            <span>
+              <Link href={'/'}> Services </Link>
+            </span>
+            <span>
+              <Link href={'/'}> Coaching </Link>
+            </span>
+            <span>
+              <Link href={'/'}> Blogs </Link>
+            </span>
+            <span>
+              <Link href={'/'}> Contact Us </Link>
+            </span>
+            <span>
+              <Link href={'/'}> Tools </Link>
+            </span>
+          </div>
+          <ThemeToggleAndHamburger />
+        </div>
+        <div
+          className="
+            border-b
+            border-[#d8d8d8]
+            h-0
+            mx-6
+            opacity-50
+            xl:mx-20
+          "
+        />
+      </nav>
+    </header>
+  );
+}
