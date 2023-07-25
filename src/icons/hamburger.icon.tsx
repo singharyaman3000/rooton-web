66a082ff82fdc3a9bafcc555bfbb6bfc99872c6c
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-07-25 23:25:19 +0530

    fixed hamburger color when theme is chnaged

--- src/icons/hamburger.icon.tsx
+++ src/icons/hamburger.icon.tsx
@@ -1,11 +1,7 @@
-type HamburgerIconProps = {
-  fill?: '#FFF' | '#000'
-}
-
-export default function HamburgerIcon({ fill = '#FFF' }: HamburgerIconProps) {
+export default function HamburgerIcon() {
   return (
-    <svg width="24" height="18" viewBox="0 0 24 18" xmlns="http://www.w3.org/2000/svg">
-      <path d="M0 0h24v2H0zm0 8h24v2H0zm0 8h24v2H0z" fill={fill} fill-rule="evenodd" />
+    <svg width="24" height="18" style={{ fill: 'var(--primary-font-color)' }} viewBox="0 0 24 18" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 0h24v2H0zm0 8h24v2H0zm0 8h24v2H0z" fillRule="evenodd" />
     </svg>
   );
 }
