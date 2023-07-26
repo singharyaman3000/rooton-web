e826eeb211285ea860f01e7392d9a2dd824079e4
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-07-27 02:22:54 +0530

    Create language dropdown

---
+++ src/icons/GermanyFlagIcon.tsx
@@ -0,0 +1,26 @@
+export default function GermanyFlagIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <defs>
+        <path id="9bsiiljzta" d="M0 0h16v16H0z" />
+      </defs>
+      <g fill="none" fill-rule="evenodd">
+        <mask id="v2kt8m564b" fill="#fff">
+          <use xlinkHref="#9bsiiljzta" />
+        </mask>
+        <use fill="#D8D8D8" xlinkHref="#9bsiiljzta" />
+        <g mask="url(#v2kt8m564b)" fill-rule="nonzero">
+          <path fill="#000" d="M-5.6 0h26.4v16H-5.6z" />
+          <path fill="#D00" d="M-5.6 5.333h26.4V16H-5.6z" />
+          <path fill="#FFCE00" d="M-5.6 10.667h26.4V16H-5.6z" />
+        </g>
+      </g>
+    </svg>
+  );
+}
