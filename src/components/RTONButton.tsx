2e715a421d927aa73bc4c6fb9cee7368c7a5be51
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-07-27 00:23:30 +0530

    Implemented responsivness

--- src/components/RTONButton.tsx
+++ src/components/RTONButton.tsx
@@ -11,9 +11,9 @@ export default function RTONButton({ onClick, text, icon }: RTONButtonProps) {
     <button
       type="button"
       onClick={onClick}
-      className=" pt-[17.8px] pl-[22.5px] pr-[19px] pb-[19.2px] flex gap-[21.5px] bg-white"
+      className=" pt-[17.8px] text-black pl-[22.5px] pr-[19px] pb-[19.2px] flex gap-[21.5px] bg-white lg:text-lg"
     >
-      <p className=" font-bold text-primary-font-color text-[13px]">{ text }</p>
+      <p className=" font-bold text-[13px] lg:text-lg">{ text }</p>
       { icon }
     </button>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'banner-grid-overlay': 'url("/banner-grid-cverlay.svg")',
       },
       colors: {
         'font-color-orange': 'var(--font-color-orange)',
