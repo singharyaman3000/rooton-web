0f74d9d97f535c20d36d8ea236323b46d6af2bf1
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-07-26 13:50:20 +0530

    theme toggle bg color fix

--- src/icons/hamburger.icon.tsx
+++ src/icons/hamburger.icon.tsx
@@ -1,7 +1,7 @@
 export default function HamburgerIcon() {
   return (
-    <svg width="24" height="18" style={{ fill: 'var(--header-font-color)' }} viewBox="0 0 24 18" xmlns="http://www.w3.org/2000/svg">
-      <path d="M0 0h24v2H0zm0 8h24v2H0zm0 8h24v2H0z" fillRule="evenodd" />
+    <svg width="24" height="18" viewBox="0 0 24 18" xmlns="http://www.w3.org/2000/svg">
+      <path fill="#FFF" d="M0 0h24v2H0zm0 8h24v2H0zm0 8h24v2H0z" fillRule="evenodd" />
     </svg>
   );
 }
