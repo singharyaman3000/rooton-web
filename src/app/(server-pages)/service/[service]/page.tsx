34cdfdc69d64a35ef0e229035d84c009c32a9695
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-07-26 23:53:02 +0530

    Create banner as resuable component

--- src/app/(server-pages)/service/[service]/page.tsx
+++ src/app/(server-pages)/service/[service]/page.tsx
@@ -1,20 +1,14 @@
+import RTONBanner from '@/components/RTONBanner';
+import BookAnAppointmentButton from '@/components/ServicePage/BookAnAppointmentButton';
+
 export default function ServicePage() {
   return (
-    <div
-      className=" w-full mt-0 h-[640px]"
-      style={{ backgroundImage: 'url(https://i.pinimg.com/736x/f5/be/fa/f5befa6a938b0cb3ca40d3b8a1fda28e.jpg)' }}
-    >
-      <div
-        className=" px-6 pb-9 w-full h-full"
-        style={{ backgroundImage: 'linear-gradient(190deg, rgb(0 0 0 / 43%) 10%, rgb(0 0 0 / 59%) 42%, #0009 10%)' }}
-      >
-        <h1 className="open-work-permit text-[37.2px] pt-[320px] mr-9 font-bold leading-[normal] font-jakartaSans">
-          Open Work Permit
-        </h1>
-        <p className=" mt-[22.2px] text-white font-bold text-[15px] leading-[1.67]">
-          Explore work opportunities in Canada with our Open Work Permit Service.
-        </p>
-      </div>
-    </div>
+    <RTONBanner
+      backgroundImageUrl="url(https://i.pinimg.com/736x/f5/be/fa/f5befa6a938b0cb3ca40d3b8a1fda28e.jpg)"
+      addGradient
+      heroText="Open Work Permit"
+      description="Explore work opportunities in Canada with our Open Work Permit Service."
+      button={<BookAnAppointmentButton />}
+    />
   );
 }
---
+++ src/components/ServicePage/BookAnAppointmentButton.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import RTONButton from '../RTONButton';
+
+export default function BookAnAppointmentButton() {
+  return <RTONButton onClick={() => {}} text="Book an appointment" icon={<i>i</i>} />;
+}
