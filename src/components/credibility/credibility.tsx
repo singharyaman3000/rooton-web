0325b87cd9fd1c81208795acabfe44231143fe98
ajeshajayanqb <ajesh.ajayan@qburst.com>
2023-07-27 23:00:40 +0530

    added animations

--- src/components/credibility/credibility.tsx
+++ src/components/credibility/credibility.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import SubTitle from '../home-contents/SubTitle';
 import TitleWrapper from '../home-contents/Title';
@@ -5,6 +7,7 @@ import Description from '../home-contents/Description';
 import ImageCard from '../UIElements/image-card';
 import CredibilityContentsJson from './credibilityContents.json';
 import Container from '../UIElements/wrapper-container';
+import { motion } from 'framer-motion';
 
 const Credibility = () => {
   interface ICredibilitycontent {
@@ -19,14 +22,27 @@ const Credibility = () => {
   return (
     <Container>
       <div className="mt-10 md:flex md:justify-between md:w-full xl:max-h-[534px]">
-        <div className="w-full md:w-[50vw] xl:w-[40vw] max-w-[576px]">
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            ease: 'easeInOut',
+            duration: 0.6,
+            delay: 0.12,
+          }}
+          className="w-full md:w-[50vw] xl:w-[40vw] max-w-[576px]"
+        >
           <SubTitle subtitle={CredibilityContent?.subtitle} />
           <TitleWrapper title={CredibilityContent?.title} />
           <Description cssClass="mt-6" description={CredibilityContent?.description} />
-        </div>
+        </motion.div>
         <div className="flex flex-col gap-6 lg:gap-[52px] mt-8 lg:mt-0 items-center md:w-[27.7vw] max-w-[399px] xl:mr-[120px] lg:h-full">
-          {CredibilityContent?.licenseImages?.map((lisenseImage) => (
+          {CredibilityContent?.licenseImages?.map((lisenseImage, index) => (
             <ImageCard
+              index={index}
               key={`${lisenseImage?.title}`}
               cssClass="h-[50.7px] sm:w-[80%] md:w-full"
               imageUrl={lisenseImage?.imageUrl}
