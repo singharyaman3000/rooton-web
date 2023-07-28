a2806d50b3fe7142434c669659357f8e38ff19da
ajeshajayanqb <ajesh.ajayan@qburst.com>
2023-07-28 15:12:23 +0530

    in progress

--- src/app/(server-pages)/page.tsx
+++ src/app/(server-pages)/page.tsx
@@ -2,8 +2,13 @@ import Credibility from '@/components/credibility/credibility';
 import BlogListings from '@/components/HomePage/BlogListings';
 import ServicesListing from '@/components/HomePage/ServicesListing';
 import Testimonials from '@/components/HomePage/Testimonials';
+import { getHomePageContents } from '../services/apiService/homeAPI';
 
-export default function Home() {
+export default async function Home() {
+  const apiRes = await getHomePageContents();
+
+  console.log(apiRes)
+
   return (
     <>
       <Credibility />
