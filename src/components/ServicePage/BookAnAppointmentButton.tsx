eb04060957a49a7ce0a34d1f2c91edd32cdd4605
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-07-28 15:15:06 +0530

    Changed icon

--- src/components/ServicePage/BookAnAppointmentButton.tsx
+++ src/components/ServicePage/BookAnAppointmentButton.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import CalenderIcon from '@/icons/CalenderIcon';
 import RTONButton from '../RTONButton';
 
 export default function BookAnAppointmentButton() {
-  return <RTONButton onClick={() => {}} text="Book an appointment" icon={<i>i</i>} />;
+  return <RTONButton onClick={() => {}} text="Book an appointment" icon={<CalenderIcon />} />;
 }
