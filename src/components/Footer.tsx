dd083943b7f865db9a3cd8fe1d27b27d43ee132a
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-07-28 16:30:23 +0530

    use link instead of pa tag

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,49 +44,49 @@ export default function Footer() {
           <div className=" flex flex-col gap-8 mb-7">
             <p className=" m-auto lg:m-0 text-sm">Follow us on</p>
             <div className=" flex gap-12 justify-center">
-              <span>
+              <Link href={'/'}>
                 <FacebookIcon />
-              </span>
-              <span>
+              </Link>
+              <Link href={'/'}>
                 <TwitterIcon />
-              </span>
-              <span>
+              </Link>
+              <Link href={'/'}>
                 <LinkedInIcon />
-              </span>
-              <span>
+              </Link>
+              <Link href={'/'}>
                 <YoutubeIcon />
-              </span>
+              </Link>
             </div>
           </div>
         </div>
         <div className=" hidden w-[1px] h-[312px] bg-primary-border lg:block ml-[128px] mr-20" />
         <div
           className="
-        flex
-        lg:flex-col
-        gap-10
-        lg:gap-4
-        flex-wrap
-        justify-between
-        text-sm
-        font-semibold
-        lg:w-[145px]
-        lg:justify-start
-      "
+            flex
+            lg:flex-col
+            gap-10
+            lg:gap-4
+            flex-wrap
+            justify-between
+            text-sm
+            font-semibold
+            lg:w-[145px]
+            lg:justify-start
+          "
         >
           <div className=" flex flex-col gap-3">
-            <p>Careers</p>
-            <p>Privacy Policy</p>
-            <p>Terms & Condition</p>
-            <p>FAQs</p>
-            <p>QnA Forum</p>
+            <Link href={'/'}>Careers</Link>
+            <Link href={'/'}>Privacy Policy</Link>
+            <Link href={'/'}>Terms & Condition</Link>
+            <Link href={'/'}>FAQs</Link>
+            <Link href={'/'}>QnA Forum</Link>
           </div>
           <div className=" flex flex-col gap-3">
-            <p>Book a Meeting RCIC</p>
-            <p>Disclaimer</p>
-            <p>GCKey vs APR </p>
-            <p>Affiliate Program</p>
-            <p>Sitemap</p>
+            <Link href={'/'}>Book a Meeting RCIC</Link>
+            <Link href={'/'}>Disclaimer</Link>
+            <Link href={'/'}>GCKey vs APR </Link>
+            <Link href={'/'}>Affiliate Program</Link>
+            <Link href={'/'}>Sitemap</Link>
           </div>
         </div>
       </div>
@@ -105,35 +105,35 @@ export default function Footer() {
           <div className=" mb-[6px]">
             <FlagCanadaIcon />
           </div>
-          <Link href={'/'} className=" text-sm mb-[4px] font-bold">
+          <p className=" text-sm mb-[4px] font-bold">
             {LAYOUT.addressCanada.title}
-          </Link>
-          <Link href={'/'} className=" text-sm mb-[4px]">
+          </p>
+          <p className=" text-sm mb-[4px]">
             {LAYOUT.addressCanada.line1}
-          </Link>
-          <Link href={'/'} className=" text-sm">
+          </p>
+          <p className=" text-sm">
             {LAYOUT.addressCanada.line2}
-          </Link>
-          <Link href={'/'} className=" text-sm mb-[4px] font-bold mt-2">
+          </p>
+          <p className=" text-sm mb-[4px] font-bold mt-2">
             Phone {LAYOUT.addressCanada.phone}
-          </Link>
+          </p>
         </div>
         <div>
           <div className=" mt-8 mb-[6px]">
             <FlagIndiaIcon />
           </div>
-          <Link href={'/'} className=" text-sm font-bold mb-[4px]">
+          <p className=" text-sm font-bold mb-[4px]">
             {LAYOUT.addressIndia.title}
-          </Link>
-          <Link href={'/'} className=" text-sm mb-[4px]">
+          </p>
+          <p className=" text-sm mb-[4px]">
             {LAYOUT.addressIndia.line1}
-          </Link>
-          <Link href={'/'} className=" text-sm">
+          </p>
+          <p className=" text-sm">
             {LAYOUT.addressIndia.line2}
-          </Link>
-          <Link href={'/'} className=" text-sm mb-[4px] font-bold mt-2">
+          </p>
+          <p className=" text-sm mb-[4px] font-bold mt-2">
             Phone {LAYOUT.addressIndia.phone}
-          </Link>
+          </p>
         </div>
       </div>
     </footer>
