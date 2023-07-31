61d17ad4f20fe07927e5e71614b9a2494e64c690
ajeshajayanqb <ajesh.ajayan@qburst.com>
2023-07-31 17:57:54 +0530

    fixed ui

--- src/components/HomePage/Honesty/index.tsx
+++ src/components/HomePage/Honesty/index.tsx
@@ -20,46 +20,44 @@ export interface IHonesty extends ITitleAttributes {
 }
 
 const Honesty = ({ json_content, title, sub_title, description }: IHonesty) => (
-  <>
-    <div className="relative pt-[73px]">
-      <Container>
-        <motion.div className="lg:flex lg:flex-row justify-between mb-11 lg:mb-14 relative">
-          <div className="mb-2.5 lg:w-[24.58vw]">
-            <SectionTitle title={title} />
-            <SubSectionTitle title={sub_title} />
-          </div>
-          <motion.div
-            transition={{
-              ease: 'easeInOut',
-              duration: 0.6,
-              delay: 0.02,
-            }}
-            className="lg:w-[56.875vw] "
-          >
-            <Description cssClass="mt-6" description={description ?? ''} />
-          </motion.div>
-        </motion.div>
-        <div className="mb-[40px]  lg:mb-[100px]">
-          <div className="honestyBackground grid grid-cols-1 border-1 border-white md:grid-cols-2 lg:grid-cols-3 gap-[1px]">
-            {json_content?.['why-rooton']?.map((whyRootOn) => {
-              return (
-                <HonestyCard
-                  key={whyRootOn?.key}
-                  title={whyRootOn?.key}
-                  value={whyRootOn.value}
-                  icon={process.env.NEXT_ASSETS_BASEURL + whyRootOn.icon}
-                  position={whyRootOn.position}
-                />
-              );
-            })}
-          </div>
+  <div className="relative pt-[73px]">
+    <Container>
+      <motion.div className="lg:flex lg:flex-row justify-between mb-11 lg:mb-14 relative">
+        <div className="mb-2.5 lg:w-[24.58vw]">
+          <SectionTitle title={title} />
+          <SubSectionTitle title={sub_title} />
         </div>
-      </Container>
-      <div className="absolute top-0 left-0 hidden lg:block">
-        <HonestyGrid />
+        <motion.div
+          transition={{
+            ease: 'easeInOut',
+            duration: 0.6,
+            delay: 0.02,
+          }}
+          className="lg:w-[56.875vw] "
+        >
+          <Description cssClass="mt-6" description={description ?? ''} />
+        </motion.div>
+      </motion.div>
+      <div className="mb-[40px]  lg:mb-[100px]">
+        <div className="honestyBackground grid grid-cols-1 border-1 border-white md:grid-cols-2 lg:grid-cols-3 gap-[1px]">
+          {json_content?.['why-rooton']?.map((whyRootOn) => {
+            return (
+              <HonestyCard
+                key={whyRootOn?.key}
+                title={whyRootOn?.key}
+                value={whyRootOn.value}
+                icon={process.env.NEXT_ASSETS_BASEURL + whyRootOn.icon}
+                position={whyRootOn.position}
+              />
+            );
+          })}
+        </div>
       </div>
+    </Container>
+    <div className="absolute top-0 left-0 hidden lg:block">
+      <HonestyGrid />
     </div>
-  </>
+  </div>
 );
 
 export default Honesty;
