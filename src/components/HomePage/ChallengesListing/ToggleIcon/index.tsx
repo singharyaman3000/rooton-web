7ba1ef4273b0079e4b4517a6bc9806ac77cca463
ajeshajayanqb <ajesh.ajayan@qburst.com>
2023-08-01 21:19:51 +0530

    added missing components

---
+++ src/components/HomePage/ChallengesListing/ToggleIcon/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export interface IToggleIcon {
+  isOpen: boolean;
+}
+
+const ToggleIcon = ({ isOpen }: IToggleIcon) => {
+  // const { theme } = useTheme();
+
+  //  const [theme , updateTheme] = useState<string| undefined>(undefined)
+
+  //  useEffect(()=>{
+  //     updateTheme(theme)
+  //  } , [theme])
+
+  return isOpen ? (
+    <svg xmlns="http://www.w3.org/2000/svg" className="challenges_toggle" width="24" height="24" viewBox="0 0 24 24">
+      <g fill="none" fillRule="evenodd">
+        <path style={{ fill: 'var(--icon-bg)' }} fillRule="nonzero" d="M0 24V0h24v24z" />
+        <path style={{ fill: 'var(--icon-path)' }} d="M12.577 12.577h6.173v-1.154H5.25v1.154h6.173z" />
+      </g>
+    </svg>
+  ) : (
+    <svg xmlns="http://www.w3.org/2000/svg" className="challenges_toggle" width="24" height="24" viewBox="0 0 24 24">
+      <g fill="none" fillRule="evenodd">
+        <path style={{ fill: 'var(--icon-bg)' }} fillRule="nonzero" d="M0 24V0h24v24z" />
+        <path
+          style={{ fill: 'var(--icon-path)' }}
+          d="M11.423 18.75h1.154v-6.173h6.173v-1.154h-6.173V5.25h-1.154v6.173H5.25v1.154h6.173z"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default ToggleIcon;
