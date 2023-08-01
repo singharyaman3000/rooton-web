f20237a6359be7135a17c8651769c536c5afe605
SofiaSQburst <sofia@qburst.com>
2023-08-01 22:51:03 +0530

    in progress

--- src/app/(client-pages)/[lang]/page.tsx
+++ src/app/(client-pages)/[lang]/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
-  return <>lang page</>;
+'use client';
+import HomePage from '@/components/HomePage';
+import { getHomePageContents } from '../../services/apiService/homeAPI';
+import useClientAPI from '@/components/UIElements/Slider/hooks/useClientAPI';
+
+export default function Home() {
+  const { data, error } = useClientAPI({ apiFn: getHomePageContents });
+
+  return data && <HomePage homePageConfig={data} />;
 }
--- src/components/UIElements/Slider/hooks/useClientAPI.ts
+++ src/components/UIElements/Slider/hooks/useClientAPI.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import { useEffect, useState } from 'react';
 
 const useClientAPI = <T>({ apiFn }: { apiFn: () => Promise<T> }) => {
