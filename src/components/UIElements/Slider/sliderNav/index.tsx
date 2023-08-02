42469edd493a28d403183c498438e89d08e4ce5f
SofiaSQburst <sofia@qburst.com>
2023-08-02 20:51:33 +0530

    fixed ui bugs

--- src/components/UIElements/Slider/sliderNav/index.tsx
+++ src/components/UIElements/Slider/sliderNav/index.tsx
@@ -13,12 +13,18 @@ const SliderNav = ({ cssClass, disable, leftNav, handleOnClick }: ISliderNav) =>
       type="button"
       onClick={() => handleOnClick()}
       aria-label={`${leftNav ? 'Previous' : 'Next'}`}
-      className={`w-11 h-11 px-[17px] py-[13px] bg-primary-white ${cssClass} ${
+      className={`w-11 h-11 px-[17px] slider-nav py-[13px] bg-white ${cssClass} ${
         leftNav ? 'rotate-180  right-[24px]   md:right-[-9px]' : 'left-[24px] md:left-[-9px]'
-      } ${disable && 'opacity-[0.5] cursor-not-allowed'}`}
+      } ${disable && 'cursor-not-allowed'}`}
     >
-      <svg xmlns="http://www.w3.org/2000/svg" width="10" height="18" viewBox="0 0 10 18">
-        <path d="M1.257 18 10 9.008 1.257 0 0 1.293l7.485 7.715L0 16.707z" fill="#000" fillRule="nonzero" />
+      <svg
+        className={`${disable && 'opacity-[0.2] cursor-not-allowed'} fill-toggle-dark-bg`}
+        xmlns="http://www.w3.org/2000/svg"
+        width="10"
+        height="18"
+        viewBox="0 0 10 18"
+      >
+        <path d="M1.257 18 10 9.008 1.257 0 0 1.293l7.485 7.715L0 16.707z" fillRule="nonzero" />
       </svg>
     </button>
   );
