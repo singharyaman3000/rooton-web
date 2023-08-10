67dab3cef01d588c1e5fd2a34938ce77b504ca7a
SofiaSQburst <sofia@qburst.com>
2023-08-11 01:10:28 +0530

    in progress

--- src/components/HomePage/Credibility/index.tsx
+++ src/components/HomePage/Credibility/index.tsx
@@ -9,6 +9,9 @@ import Container from '../../UIElements/wrapper-container';
 import SectionTitle from '../../UIElements/SectionHeadings/SectionTitle';
 import SubSectionTitle from '../../UIElements/SectionHeadings/SubSectiontitle';
 import CredibilityGrid from './CredibilityGrid';
+import SliderNav from '@/components/UIElements/Slider/sliderNav';
+import useSliderData from '@/components/UIElements/Slider/hooks/useSliderData';
+import useSliderPagination from '@/components/UIElements/Slider/hooks/useSlider';
 
 interface ICredibilitycontent {
   sub_title: string;
@@ -18,6 +21,22 @@ interface ICredibilitycontent {
 }
 
 const Credibility = ({ description, title, sub_title, media_url }: ICredibilitycontent) => {
+  const { pageNum, incrementPage, decrementPage } = useSliderPagination({
+    slidesLength: media_url.data.length ? Math.ceil(media_url.data.length - 1 / 2) : 0,
+    initialPage: 0,
+  });
+
+  const isVisible = (index: number) => {
+    const isLastPage = Math.ceil(media_url.data.length / 2);
+    if (pageNum + 1 === isLastPage) {
+      return index === pageNum * 2 - 1 || index === pageNum * 2;
+    } else {
+      const currentIndex = pageNum * 2;
+      const nextIndex = pageNum * 2 + 1;
+      return currentIndex === index || nextIndex === index || currentIndex + 1 === media_url.data.length;
+    }
+  };
+
   return (
     <div className="relative overflow-x-hidden overflow-y-hidden">
       <Container>
@@ -33,28 +52,55 @@ const Credibility = ({ description, title, sub_title, media_url }: ICredibilityc
               duration: 0.6,
               delay: 0.12,
             }}
-            className="w-full md:w-[50vw] xl:w-[40vw] max-w-[576px]"
+            className="w-full md:w-[45%]  max-w-[576px]"
           >
             <SectionTitle title={title} />
             <SubSectionTitle title={sub_title} />
             <Description cssClass="mt-6" description={description} />
           </motion.div>
-          <div className="z-10 mx-auto lg:mx-0 flex flex-col gap-6 lg:gap-[52px] mt-8 lg:mt-0 items-center md:w-[27.7vw] max-w-[399px] xl:mr-[120px] lg:h-full">
+          <div className="z-10 mx-auto relative  md:!h-[520px]  md:w-[43.8%] md:mr-[40px]  mt-8 lg:mt-0 items-center  lg:h-full">
             {media_url.data?.map((lisenseImage, index) => {
               return (
-                <ImageCard
-                  index={index}
+                <motion.div
+                  initial={{ opacity: 0, x: index % 2 === 0 ? 100 : -100 }}
+                  whileInView={{
+                    opacity: isVisible(index) ? 1 : 0,
+                    x: isVisible(index) ? 0 : index % 2 === 0 ? 100 : -100,
+                  }}
+                  transition={{
+                    ease: 'easeInOut',
+                    duration: 0.6,
+                    delay: 0.12,
+                  }}
+                  className={`w-full  ${index % 2 === 0 ? 'top-0' : 'bottom-0'} absolute `}
                   key={`${lisenseImage?.id}`}
-                  cssClass='h-[50.7px] sm:w-[80%] md:w-full'
-                  borderClass={`${lisenseImage?.attributes?.name}`}
-                  imageUrl={lisenseImage?.attributes.url}
-                  sizes={'30vw'}
-                  title={lisenseImage?.attributes.caption ?? ''}
-                  altText={lisenseImage?.attributes.alternativeText ?? ''}
-                  iconClass={''}
-                />
+                >
+                  {index}
+                  <ImageCard
+                    index={index}
+                    key={`${lisenseImage?.id}`}
+                    cssClass={`h-[50.7px] sm:w-[80%] md:h-[16.7vw] mx-auto my-0 flex item-center`}
+                    borderClass={`${lisenseImage?.attributes?.name}`}
+                    imageUrl={lisenseImage?.attributes.url}
+                    sizes={'30vw'}
+                    title={lisenseImage?.attributes.caption ?? ''}
+                    altText={lisenseImage?.attributes.alternativeText ?? ''}
+                    iconClass={''}
+                  />
+                </motion.div>
               );
             })}
+            <SliderNav
+              disable={pageNum === 0}
+              cssClass="absolute top-[46%] !right-[unset] -left-[40px]"
+              leftNav
+              handleOnClick={() => decrementPage()}
+            />
+            <SliderNav
+              disable={pageNum === Math.ceil(media_url.data.length - 1 / 2)}
+              cssClass="absolute top-[46%] -right-[40px] !left-[unset]"
+              handleOnClick={() => incrementPage()}
+            />
           </div>
         </div>
       </Container>
--- src/components/UIElements/image-card/index.tsx
+++ src/components/UIElements/image-card/index.tsx
@@ -24,18 +24,18 @@ const ImageCard = ({
 }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, x: index % 2 === 0 ? 100 : -100 }}
-      whileInView={{
-        opacity: 1,
-        x: 0,
-      }}
-      transition={{
-        ease: 'easeInOut',
-        duration: 0.6,
-        delay: 0.12,
-      }}
-      viewport={{ once: true }}
-      className='h-[120px] md:h-[240px] w-[86.666vw] max-w-[408.19px] shadow-lg md:w-full bg-white  border flex flex-col justify-center relative'
+      // initial={{ opacity: 0, x: index % 2 === 0 ? 100 : -100 }}
+      // whileInView={{
+      //   opacity: 1,
+      //   x: 0,
+      // }}
+      // transition={{
+      //   ease: 'easeInOut',
+      //   duration: 0.6,
+      //   delay: 0.12,
+      // }}
+      // viewport={{ once: true }}
+      className='shadow-lg md:w-full bg-white  border flex flex-col justify-center relative'
     >
       <div
         className={`${iconClass} ${borderClass?.replace('.png', '')} absolute top-0 right-0 mt-[13.6px] mr-[13.6px] lg:mt-[20px] lg:mr-[20px] border-l-transparent border-t-[15px] border-l-[15px] lg:border-l-[22px]  lg:border-t-[22px] w-[15px] h-[15px] lg:w-[22px] lg:h-[22px]`}
