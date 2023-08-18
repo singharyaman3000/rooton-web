5fefa2daf5049b2303fee62dd06f68d130a291d0
sofia <sofia@yeswearemad.com>
2023-08-18 16:07:01 +0530

    add service page null data handling

--- src/components/HomePage/OurProcess/Processes/ProcessesMobileView/index.tsx
+++ src/components/HomePage/OurProcess/Processes/ProcessesMobileView/index.tsx
@@ -19,27 +19,29 @@ const ProcessesMobileView = ({ process }: IOurProcessData) => {
       {process.map(({ key, value, position }, index) => {
         const selectedItem = selectedElem.toString() === `process-mobile-${position}`;
         return (
-          <Fragment key={`${position}-mobile`}>
-            <div
-              className="p-[16px_24px] -ml-[24px] -mr-[24px]"
-              id={`process-mobile-${position}`}
-              ref={refs[index]}
-              style={{ background: selectedItem ? 'var(--selector-bg)' : '' }}
-            >
-              <div className="w-full flex items-center mb-[8px]">
-                <span className="text-2xl text-golden-yellow mr-3  font-light not-italic leading-[normal] tracking-[normal]">
-                  {position}
-                </span>{' '}
-                <h4 className="text-sm font-bold text-primary-font-color not-italic leading-normal tracking-[normal]">
-                  {key}
-                </h4>
+          value && (
+            <Fragment key={`${position}-mobile`}>
+              <div
+                className="p-[16px_24px] -ml-[24px] -mr-[24px]"
+                id={`process-mobile-${position}`}
+                ref={refs[index]}
+                style={{ background: selectedItem ? 'var(--selector-bg)' : '' }}
+              >
+                <div className="w-full flex items-center mb-[8px]">
+                  <span className="text-2xl text-golden-yellow mr-3  font-light not-italic leading-[normal] tracking-[normal]">
+                    {position}
+                  </span>{' '}
+                  <h4 className="text-sm font-bold text-primary-font-color not-italic leading-normal tracking-[normal]">
+                    {key}
+                  </h4>
+                </div>
+                <p className="text-[13px] font-medium not-italic leading-[1.62] tracking-[normal] text-primary-font-color opacity-70">
+                  {value}
+                </p>
               </div>
-              <p className="text-[13px] font-medium not-italic leading-[1.62] tracking-[normal] text-primary-font-color opacity-70">
-                {value}
-              </p>
-            </div>
-            <MobileDivider cssClass={`${selectedItem ? 'hidden' : 'block'}`} />
-          </Fragment>
+              <MobileDivider cssClass={`${selectedItem ? 'hidden' : 'block'}`} />
+            </Fragment>
+          )
         );
       })}
     </div>
