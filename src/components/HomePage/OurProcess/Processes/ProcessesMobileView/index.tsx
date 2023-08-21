08bd4c39c7bd2a568df9b1460a787269f1881a4e
SofiaSQburst <sofia@qburst.com>
2023-08-21 11:14:19 +0530

    fixed build

--- src/components/HomePage/OurProcess/Processes/ProcessesMobileView/index.tsx
+++ src/components/HomePage/OurProcess/Processes/ProcessesMobileView/index.tsx
@@ -16,10 +16,10 @@ const ProcessesMobileView = ({ process }: IOurProcessData) => {
 
   return (
     <div className="block md:hidden mt-[34px] mb-[80px] processes-mobile-view">
-      {process.map(({ key, value, position }, index) => {
+      {process.map(({ title, description, position }, index) => {
         const selectedItem = selectedElem.toString() === `process-mobile-${position}`;
         return (
-          value && (
+          description && (
             <Fragment key={`${position}-mobile`}>
               <div
                 className="p-[16px_24px] -ml-[24px] -mr-[24px]"
@@ -32,11 +32,11 @@ const ProcessesMobileView = ({ process }: IOurProcessData) => {
                     {position}
                   </span>{' '}
                   <h4 className="text-sm font-bold text-primary-font-color not-italic leading-normal tracking-[normal]">
-                    {key}
+                    {title}
                   </h4>
                 </div>
                 <p className="text-[13px] font-medium not-italic leading-[1.62] tracking-[normal] text-primary-font-color opacity-70">
-                  {value}
+                  {description}
                 </p>
               </div>
               <MobileDivider cssClass={`${selectedItem ? 'hidden' : 'block'}`} />
