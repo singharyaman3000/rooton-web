1de423000f98508b13c4754e0827de6c257f91c0
sofia <sofia@yeswearemad.com>
2023-08-31 10:09:58 +0530

    add header active border.

--- src/app/(server-pages)/layout.tsx
+++ src/app/(server-pages)/layout.tsx
@@ -6,20 +6,17 @@ import { HeaderFooterDataProvider } from '@/providers/headerFooterDataProvider';
 import IModalShowContextProvider from '@/providers/coreServicesMOdalOpenContext';
 import ServiceListingOnAdviceMobile from '@/components/HomePage/CoreServiceListing/ServiceListOnAdviceMobile';
 import ServicesPopUpModal from '@/components/HomePage/CoreServiceListing/ServicesPopUpModal';
-import MobileModalShowContextProvider from '@/providers/coreServicesModalMobileContext';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const apiRes = await getHeaderFooterData();
   return (
     <HeaderFooterDataProvider headerFooterAPIData={apiRes}>
       <IModalShowContextProvider>
-        <MobileModalShowContextProvider>
-          <ServiceListingOnAdviceMobile />
-          <ServicesPopUpModal />
-          <Header />
-          {children}
-          <Footer />
-        </MobileModalShowContextProvider>
+        <ServiceListingOnAdviceMobile />
+        <ServicesPopUpModal />
+        <Header />
+        {children}
+        <Footer />
       </IModalShowContextProvider>
     </HeaderFooterDataProvider>
   );
