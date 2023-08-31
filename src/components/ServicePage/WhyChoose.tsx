f83d65d0dec639ea6772bd91a20786c27ba2dba9
sofia <sofia@yeswearemad.com>
2023-08-31 15:47:50 +0530

    fix image stretch issue. why choose root on.

--- src/components/ServicePage/WhyChoose.tsx
+++ src/components/ServicePage/WhyChoose.tsx
@@ -66,7 +66,7 @@ export const WhyChoose = ({ title, description, imageAlt, imageUrl, onClickCTA }
         </div>
       </div>
       <div className="
-        aspect-square lg:aspect-auto relative lg:flex-shrink-0 sm:w-[380px] lg:min-h-[560px] lg:w-[480px] h-auto
+        aspect-square lg:aspect-auto relative lg:flex-shrink-0 sm:w-[380px] lg:min-h-[560px] xl:w-[480px] h-auto
       ">
         <NextImage src={imageUrl} altText={imageAlt} sizes="30vw" title="??" fill style={{ objectFit: 'cover' }} />
       </div>
