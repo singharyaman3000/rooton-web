faa54f2fc81d63add92c7d9ffbe0a4be767c1bf5
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-09-05 15:47:05 +0530

    page size changed

--- src/app/services/apiService/apiUrl/servicePage.ts
+++ src/app/services/apiService/apiUrl/servicePage.ts
@@ -5,6 +5,6 @@ export const getServiceAPIUrl = (serviceId: unknown) => {
   return SERVICE_API.replace('<service-id>', serviceId as string);
 };
 
-export const GET_BLOGS_SERVICE = '/api/blogs/?populate[0]=media_url&populate[1]=author_profile_image.media_url&populate[2]=blog_contents.media_url&pagination[page]=1&pagination[pageSize]=3&filters[service_type][$eq]=<service-type>&filters[category][$eq]=blog';
+export const GET_BLOGS_SERVICE = '/api/blogs/?populate[0]=media_url&populate[1]=author_profile_image.media_url&populate[2]=blog_contents.media_url&pagination[page]=1&pagination[pageSize]=20&filters[service_type][$eq]=<service-type>&filters[category][$eq]=blog';
 
-export const GET_BLOGS_HOME = '/api/blogs/?populate[0]=media_url&populate[1]=author_profile_image.media_url&populate[2]=blog_contents.media_url&pagination[page]=1&pagination[pageSize]=3&filters[category][$eq]=news';
+export const GET_BLOGS_HOME = '/api/blogs/?populate[0]=media_url&populate[1]=author_profile_image.media_url&populate[2]=blog_contents.media_url&pagination[page]=1&pagination[pageSize]=20&filters[category][$eq]=news';
