3071464e0855f085e3dce8739379266dd4f113b9
Ajesh Ajayan <ajesh.ajayan@qburst.com>
2023-09-06 17:05:57 +0530

    Honesty section font size fixed

--- src/components/HomePage/Honesty/honestyCard.tsx
+++ src/components/HomePage/Honesty/honestyCard.tsx
@@ -15,8 +15,8 @@ const HonestyCard = ({ title, icon, value }: IWhyRootON) => {
       <div className="relative mb-[14px] md:mb-[21px] w-[37px] h-[37px]">
         <NextImage sizes={'30vw'} src={icon} title={title} fill style={{ objectFit: 'contain' }} altText={title} />
       </div>
-      <h1 className="text-primary-font-color line-clamp-3 w-[80%] mb-4 text-xl font-bold leading-6 tracking-normal">{title} </h1>
-      <p className="mb- text-sm font-[500] leading-[1.71] tracking-normal text-font-color-light-gray">{value}</p>
+      <h1 className="text-primary-font-color line-clamp-3 w-[80%] mb-4 text-[22px] leading-[1.36] font-bold leading-6 tracking-normal">{title} </h1>
+      <p className="mb- text-base font-[500] leading-[1.71] tracking-normal text-font-color-light-gray">{value}</p>
     </div>
   );
 };
--- src/components/HomePage/Honesty/index.tsx
+++ src/components/HomePage/Honesty/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-body-style */
 'use client';
 
 import React from 'react';
