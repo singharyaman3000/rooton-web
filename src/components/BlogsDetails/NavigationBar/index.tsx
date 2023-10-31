7d80feeae6fdb7ae07679e4bc9e4b5ce5ed86611
Tom Ramesh <tom.ramesh@qburst.com>
2023-10-31 09:35:03 +0530

    added scrolling of sections

--- src/components/BlogsDetails/NavigationBar/index.tsx
+++ src/components/BlogsDetails/NavigationBar/index.tsx
@@ -1,21 +1,69 @@
-import { IBlogContentData } from '@/app/services/apiService/blogDetailAPI';
+'use client';
 
-const NavigationPanel = ({ content }: { content: IBlogContentData[] }) => {
-  console.log('-----------', content);
+import { IBlogContentData } from '@/app/services/apiService/blogDetailAPI';
+import React, { Dispatch, RefObject, SetStateAction, useEffect, useMemo, useState } from 'react';
+import { SelectedTagType } from '..';
+
+type NavigationPanelPropsType = {
+  content: IBlogContentData[];
+  selectedTag: SelectedTagType;
+  setSelectedTag: Dispatch<SetStateAction<SelectedTagType>>;
+};
+
+const useSelectedTabPosition = (currentRef: RefObject<HTMLSpanElement>) => {
+  const [fromTop, setFromTop] = useState(0);
+
+  useEffect(() => {
+    if (currentRef?.current) {
+      console.log(currentRef.current.getBoundingClientRect().top, currentRef.current.offsetHeight);
+      setFromTop(currentRef.current.offsetHeight / 2 - 5);
+    }
+  }, [currentRef?.current]);
+
+  return { fromTop };
+};
+
+const NavigationPanel: React.FC<NavigationPanelPropsType> = ({ content, selectedTag, setSelectedTag }) => {
+  const refs = useMemo(() => {
+    return content?.map(() => {
+      return React.createRef<HTMLSpanElement>();
+    });
+  }, []);
+  const { fromTop } = useSelectedTabPosition(selectedTag.activeRef);
+console.log(fromTop);
 
   return (
     <div className="w-[600px] h-full px-20 bg-slate-300 flex items-center justify-center">
       <div className="fixed top-[25%]">
         <h3 className="font-bold text-xl mb-5">In this article</h3>
-        <nav className="flex flex-col gap-5">
-          {content.map(({ id, attributes }) => {
-            return (
-              <a key={id} href={`#position-${id}`} className="max-w-[320px] hover:font-bold">
-                {attributes.title}
-              </a>
-            );
-          })}
-        </nav>
+        <div className="flex gap-2">
+          <div className="relative w-[1px] bg-[#d7d7d7]">
+            {/* <span className="block w-[1px] h-full bg-[#d7d7d7]"></span> */}
+            <span
+              className="absolute block w-1 h-10 bg-black left-0 transform -translate-x-1/2"
+              style={{ top: fromTop }}
+            ></span>
+          </div>
+          <nav className="flex flex-col gap-5 align-text-top">
+            {content.map((contentData, index) => {
+              return (
+                <span
+                  id={`position-${contentData.id}`}
+                  key={contentData.id}
+                  ref={refs[index]}
+                  className="max-w-[320px] hover:font-bold"
+                  onClick={() => {
+                    setSelectedTag({ tag: contentData, activeRef: refs[index] });
+                  }}
+                  role="button"
+                  tabIndex={index + 1}
+                >
+                  {contentData.attributes.title}
+                </span>
+              );
+            })}
+          </nav>
+        </div>
       </div>
     </div>
   );
--- src/components/BlogsDetails/index.tsx
+++ src/components/BlogsDetails/index.tsx
@@ -1,13 +1,25 @@
 'use client';
 
-import { IBlogDetails } from '@/app/services/apiService/blogDetailAPI';
+import { IBlogContentData, IBlogDetails } from '@/app/services/apiService/blogDetailAPI';
 import { Breadcrumbs } from '../Breadcrumbs';
 import NavigationPanel from './NavigationBar';
+import { RefObject, useEffect, useState } from 'react';
+
+export type SelectedTagType = { tag: IBlogContentData; activeRef: RefObject<HTMLSpanElement> };
 
 const BlogDetails = ({ details }: { details: IBlogDetails }) => {
+  const [selectedSection, setSelectedSection] = useState<SelectedTagType>({} as SelectedTagType);
+
   const sortedContent = details?.attributes?.blog_contents?.data?.toSorted((a, b) => {
     return (a?.attributes?.position ?? 0) - (b?.attributes?.position ?? 0);
   });
+
+  useEffect(() => {
+    if (selectedSection?.activeRef) {
+      const section = document.querySelector(`#section-container #${selectedSection.activeRef.current?.id}`);
+      if (section) section.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    }
+  }, [selectedSection]);
 
   return (
     <div className="mt-20 text-primary-font-color flex flex-col">
@@ -31,8 +43,8 @@ const BlogDetails = ({ details }: { details: IBlogDetails }) => {
         />
       </div>
       <div className="flex">
-        <NavigationPanel content={sortedContent} />
-        <main className="w-full scroll-smooth">
+        <NavigationPanel content={sortedContent} selectedTag={selectedSection} setSelectedTag={setSelectedSection} />
+        <div id="section-container" className="w-full">
           <section id="position-9" className="h-[300px] w-full bg-blue-400">
             Position 9
           </section>
@@ -45,7 +57,16 @@ const BlogDetails = ({ details }: { details: IBlogDetails }) => {
           <section id="position-12" className="h-[300px] w-full bg-yellow-400">
             Position 12
           </section>
-        </main>
+          <section id="position-13" className="h-[300px] w-full bg-gray-400">
+            Position 13
+          </section>
+          <section id="position-14" className="h-[300px] w-full bg-orange-400">
+            Position 14
+          </section>
+          <section id="position-15" className="h-[300px] w-full bg-cyan-400">
+            Position 15
+          </section>
+        </div>
       </div>
     </div>
   );
