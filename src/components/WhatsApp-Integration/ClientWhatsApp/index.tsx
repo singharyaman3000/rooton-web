c1c5224123cb4260854f466dab755454ab50ab18
DhrumilP2905 <dhrumil.panchal@rooton.ca>
2023-11-03 17:11:30 +0530

    resolved build error

--- src/components/WhatsApp-Integration/ClientWhatsApp/index.tsx
+++ src/components/WhatsApp-Integration/ClientWhatsApp/index.tsx
@@ -1,16 +1,25 @@
 import { ClientContainer } from '@/components/Containers/ClientContainer';
 import React, { useEffect, useState } from 'react';
 import  WhatsAppButton from '@/components/WhatsApp-Integration';
-import { getHeaderFooterData } from '@/app/services/apiService/headerFooterAPI';
+import { getHeaderFooterData, IWhatsAppAttributes } from '@/app/services/apiService/headerFooterAPI';
+
+
+export interface IWhatsAppProps {
+  whatsapp: IWhatsAppAttributes;
+  hideTemplate: () => void;
+  showTypingInitial: boolean;
+}
 
 const ClientWhatsAppButton = () => {
-  const [whatsAppData, setWhatsAppData] = useState(null);
+  const [whatsAppData, setWhatsAppData] = useState<IWhatsAppAttributes | null>(null);
+
 
   useEffect(() => {
     const fetchData = async () => {
       const headerFooterData = await getHeaderFooterData();
 
-      setWhatsAppData(headerFooterData[0]?.attributes?.whats_app?.data?.attributes);
+      setWhatsAppData(headerFooterData[0]?.attributes?.whats_app?.data?.attributes ?? null);
+
     };
 
     fetchData();
