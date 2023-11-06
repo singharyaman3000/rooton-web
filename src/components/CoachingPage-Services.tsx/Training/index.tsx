03c442449c2e78582c26f7424bd93d9bce565553
Marmik patel <62463824+marmik28@users.noreply.github.com>
2023-11-06 12:45:23 +0530

    training css code changes

--- src/components/CoachingPage-Services.tsx/Training/index.tsx
+++ src/components/CoachingPage-Services.tsx/Training/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ITraining } from '@/app/services/apiService/coaching_contentsAPI';
 import { appendAssetUrl } from '@/utils';
+import styles from './training.module.css'; // Ensure the path is correct
 
 type TrainingCardProps = {
   training: ITraining;
@@ -11,110 +12,25 @@ type TrainingCardProps = {
 const TrainingCard: React.FC<TrainingCardProps> = ({ training, isFirst, index }) => {
   return (
     <div
-      className={`card training-cards flex flex-row bg-secondary-grey relative my-5 ${isFirst ? 'first-card' : ''} ${
-        index === 1 ? 'second-card' : ''
+      className={`card ${styles.trainingCards} flex flex-row bg-secondary-grey relative my-5 ${isFirst ? styles.firstCard : ''} ${
+        index === 1 ? styles.secondCard : ''
       }`}
     >
-      <style jsx>{`
-        .card {
-          min-width: 350px;
-          width: 100%;
-          padding: 40px;
-          margin-right: 30px;
-          margin-top: 41px;
-          border-radius: 8px;
-        }
-        // .card::-webkit-scrollbar {
-        //   display: none;
-        // }
-        // .card {
-        //   scrollbar-width: none;
-        //   -ms-overflow-style: none;
-        // }
-
-        .first-card {
-          border-top: none;
-          background-color: #fff;
-          padding: 0px;
-          box-shadow: none;
-          display: none;
-        }
-
-        .title {
-          display: flex; 
-          margin: auto; 
-          justify-content: center; 
-             
-        }
-        .image {
-          height: 35px;
-          position: relative;
-          top: -5px;
-          left: -15px;
-        }
-
-        .card-text1 {
-          font-size: 15px;
-          font-weight: 500;
-          letter-spacing: 1px;
-          min-height: 250px;
-          max-height: 100%;
-          // text-align: justify;         
-        }
-        
-
-        .subtitle {
-          font-size: 20px;
-          font-weight: 400;
-          margin-top: 12px;
-        }
-        .subtitle1 {
-          font-size: 23px;
-          font-weight: 600;
-          width: 100%;
-        }
-        .description_container {
-          display: flex;
-          flex-direction: column;
-        }
-
-        .heading {
-          text-align: center;
-          font-size: 20px;
-          font-weight: bold;
-          letter-spacing: 0.5px;
-        }
-
-        @media (max-width: 768px) {
-          .card-text1 {
-            font-size: 13px;
-          }
-        }
-
-        @media (max-width: 480px) {
-          .card-text1 {
-            font-size: 12px;
-          }
-          .card {
-            min-width: 300px;
-            max-width: 100%;
-            padding: 40px;
-          }
-        }
-      `}</style>
-
+      
       {isFirst ? (
-        <div className="description_container first-card">
-          {/* <h1 className="subtitle1 mb-2">{training.title}: </h1> */}
-          {/* <h2 className="subtitle">{training.description}</h2> */}
+        <div className={styles.descriptionContainer}>
+          {/* <h1 className={styles.subtitle1}> */}
+          {/* {training.title}: </h1> */}
+          {/* <h2 className={styles.subtitle}> */}
+          {/* {training.description}</h2> */}
         </div>
       ) : (
-        <div className=" second-card flex flex-row">
-          <div className="title xyz xyyz w-full">
-            <img className='image' src={appendAssetUrl(training.image)} alt="boy" />
-            <h2 className="heading qqq text-ali text-lg sm:text-xl md:text-2xl font-bold mb-2">{training.title}</h2>
+        <div className={`second-card flex flex-row`}>
+          <div className={`title ${styles.xyz} ${styles.xyyz} w-full`}>
+            <img className={styles.image} src={appendAssetUrl(training.image)} alt="boy" />
+            <h2 className={`heading ${styles.qqq} text-ali text-lg sm:text-xl md:text-2xl font-bold mb-2`}>{training.title}</h2>
           </div>
-          <div className="card-text1 card-text">
+          <div className={`card-text1 ${styles.cardText}`}>
             <p>{training.description}</p>
           </div>
         </div>
