7a28b8119aeb6cd17fae07798352544879484ea0
Tom Ramesh <tom.ramesh@qburst.com>
2023-11-13 16:21:00 +0530

    added mobile responsive view

--- src/components/ContactUsPage/SocialSection/index.tsx
+++ src/components/ContactUsPage/SocialSection/index.tsx
@@ -1,18 +1,31 @@
+import { useRef } from 'react';
+import BookAppointmentForm from '@/components/AboutUsPage/BookAppointmentForm';
+import { BOOK_APPOINTMENT_FORM_META } from '../constants';
+
 const SocialSection = () => {
+  const bookRef = useRef<HTMLElement>(null);
   const p1 =
     'In recent years, social media has enhanced the work of our real estate agents, that are now able to showcase the properties they represent on multiple channels.';
   const p2 =
     'Be the first to find out about the most recent estates we are renting and/or selling and follow is on social media!.';
 
   return (
-    <section className="flex px-6 lg:px-0">
-      <div className="text-sm lg:text-base pt-[68px] pb-[51px]">
+    <section className="flex flex-col lg:flex-row">
+      <div className="text-sm px-6 lg:w-[50%] lg:text-base pb-[51px] pt-[68px] lg:pb-0 lg:pt-0 flex flex-col justify-center">
         <h2 className="text-[28px] lg:text-[40px] mb-10 font-extrabold">{"Let's be social"}</h2>
         <div></div>
-        <p>{p1 + p2}</p>
+        <p className="max-w-[469px]">{p1 + p2}</p>
       </div>
       {/* Form section */}
-      <div></div>
+      <div className="contact-us-form-container px-6 lg:px-0">
+        <BookAppointmentForm
+          sectionRef={bookRef}
+          displayBookAppointment
+          formData={BOOK_APPOINTMENT_FORM_META.formData}
+          formHeading={BOOK_APPOINTMENT_FORM_META.formHeading}
+          imageUrl={BOOK_APPOINTMENT_FORM_META.imageUrl}
+        />
+      </div>
     </section>
   );
 };
--- src/components/ContactUsPage/index.tsx
+++ src/components/ContactUsPage/index.tsx
@@ -2,6 +2,7 @@
 
 import RTONBanner from '../RTONBanner';
 import BookAnAppointmentButton from '../ServicePage/BookAnAppointmentButton';
+import BookAnAppointment from '../UIElements/BookAnAppointment';
 import MapSection from './MapSection';
 import SocialSection from './SocialSection';
 import { BREAD_BRUMB_PATHS } from './constants';
@@ -22,8 +23,11 @@ const ContactUs = () => {
         subDescription="Book 1:1 Paid Consultation with RCIC!"
         button={<BookAnAppointmentButton text="Book an Appoinment now" onClick={() => {}} />}
       />
-      <SocialSection/>
-      <MapSection/>
+      <div className='lg:p-20'>
+        <SocialSection />
+        <MapSection />
+        <BookAnAppointment onClick={() => {}} />
+      </div>
     </>
   );
 };
