b2f70cc270cf95d74051ec680fffd00039788cf6
geethu.g <geethu.g@qburst.com>
2023-11-16 19:27:04 +0530

    Icons cut off issue fix in home page

--- src/components/HomePage/ServicesListing/ServicesMobileView/AccordionHeader/index.tsx
+++ src/components/HomePage/ServicesListing/ServicesMobileView/AccordionHeader/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const AccordionHeader = ({ service, icon , cssClass }: { service: string; icon: string , cssClass?:string }) => {
   return (
     <div className={`flex items-center ${cssClass}`}>
-      <div className="relative mr-3 square-[30px] mb-[8px] text-base not-italic leading-[normal] tracking-[normal] text-black">
+      <div className="relative mr-3 square-[30px] mb-0 text-base not-italic leading-[normal] tracking-[normal] text-black">
         {icon && (
           <NextImage
             sizes="100vw"
@@ -12,7 +12,7 @@ export const AccordionHeader = ({ service, icon , cssClass }: { service: string;
             title={`${service}-icon`}
             src={icon}
             fill
-            style={{ objectFit: 'cover' }}
+            style={{ objectFit: 'contain' }}
           />
         )}
       </div>
