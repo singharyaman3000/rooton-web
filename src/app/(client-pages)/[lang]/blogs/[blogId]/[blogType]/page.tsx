0d49c6e98f87d74062334def774a2337bfd270c3
Tom Ramesh <tom.ramesh@qburst.com>
2023-11-30 12:58:39 +0530

    added client side meta info, updated all language urls in sitemap.xml

--- src/app/(client-pages)/[lang]/blogs/[blogId]/[blogType]/page.tsx
+++ src/app/(client-pages)/[lang]/blogs/[blogId]/[blogType]/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { IBlogDetails, getBlogDetails } from '@/app/services/apiService/blogDetailAPI';
+import { getBlogMetaInfo } from '@/app/services/apiService/blogMetaInfo';
 import { ArticleCategoryType } from '@/app/services/apiService/blogsListAPI';
 import BlogDetails from '@/components/BlogsDetails';
 import LoadingUI from '@/components/LoadingUI';
 import useClientAPI from '@/components/UIElements/Slider/hooks/useClientAPI';
+import useSetMetaInfo from '@/hooks/useSetMetaInfo';
 import { useTranslationLoader } from '@/providers/translationLoadingProvider';
 import { useParams } from 'next/navigation';
 
@@ -16,7 +18,16 @@ const BlogsDetailsPageCSR = () => {
       return getBlogDetails(params.blogId);
     },
   });
+
+  const { data: metaData = [] } = useClientAPI({
+    apiFn: () => {
+      return getBlogMetaInfo(params.blogId);
+    },
+  });
   const { loader } = useTranslationLoader();
+
+  const canonicalUrl = `https://rooton.ca/${params?.lang}/blogs/${params?.blogId}/${params?.blogType}`;
+  useSetMetaInfo(metaData![0]?.attributes?.meta_title ?? '', metaData![0]?.attributes?.meta_description, canonicalUrl);
 
   if (loader || loading) return <LoadingUI />;
   const isFromCoachingPage = params?.blogType === 'coaching-tips';
