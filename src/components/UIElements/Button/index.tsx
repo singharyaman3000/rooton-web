9ff3683005b25df0c9c0dc7b0795917d7ef28f6f
Aromal <aromal@qburst.com>
2023-12-06 16:05:53 +0530

    Fix Tooltip being applied globally For button Get Queries resolved

--- src/components/UIElements/Button/index.tsx
+++ src/components/UIElements/Button/index.tsx
@@ -1,4 +1,3 @@
-
 import React, { ReactNode } from 'react';
 
 export interface IButton {
@@ -8,10 +7,11 @@ export interface IButton {
   ariaLabel: string;
   icon?: ReactNode;
   cssClass?: string;
+  toolTip?: boolean;
 }
 
-const Button = ({ ariaLabel, label, handleOnClick, tabIndex = 0, icon, cssClass }: IButton) => {
-  return (
+const Button = ({ ariaLabel, label, handleOnClick, tabIndex = 0, icon, cssClass, toolTip = false }: IButton) => {
+  return toolTip ? (
     <button
       type="button"
       data-tooltip
@@ -20,7 +20,24 @@ const Button = ({ ariaLabel, label, handleOnClick, tabIndex = 0, icon, cssClass 
       onClick={handleOnClick}
       tabIndex={tabIndex}
     >
-      <p className='truncate'>{label}</p>
+      <p className="truncate">{label}</p>
+      <div className="ml-[12px]">
+        {icon ?? (
+          <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10">
+            <path d="M1.187 10 0 8.813l7.109-7.109H.786V0H10v9.214H8.296V2.891z" fill="#FFAF00" fillRule="nonzero" />
+          </svg>
+        )}
+      </div>
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={`${cssClass} p-[8px_12px] md:p-[10px_16px] border border-solid border-deep-yellow flex items-center text-xs font-medium not-italic leading-[normal] tracking-[normal] md:text-sm`}
+      aria-label={label ?? ariaLabel}
+      onClick={handleOnClick}
+      tabIndex={tabIndex}
+    >
+      {label}
       <div className="ml-[12px]">
         {icon ?? (
           <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10">
