8085a25b48aaf6b965c08130aa3bb82959ed14b6
Aromal <aromal@qburst.com>
2023-12-11 16:17:01 +0530

    Fix RW-314: Text jumping to top on page refresh

--- src/components/ContactUsPage/SocialSection/index.tsx
+++ src/components/ContactUsPage/SocialSection/index.tsx
@@ -72,7 +72,7 @@ const SocialSection = ({
           imageUrl={appoinmentForm.imageUrl}
           ctaClickSource={ctaClickSource}
           formHeadingCss='!font-bold !text-2xl md:!text-[32px]'
-          formContainerCss='!pt-5 md:!pt-10'
+          formContainerCss='!pt-5 md:!pt-10 xl:min-h-[708px]'
         />
       </div>
     </section>
