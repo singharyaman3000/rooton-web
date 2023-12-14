4df8dbe51a7714626a1a63acb044b436822c0479
Tom Ramesh <tom.ramesh@qburst.com>
2023-12-14 20:45:12 +0530

    removed pagination for banner api

--- src/app/services/apiService/apiUrl/homePage/index.tsx
+++ src/app/services/apiService/apiUrl/homePage/index.tsx
@@ -5,7 +5,7 @@ export const HEADER_FOOTER_API = '/api/commons?populate[0]=addresses.media_url&p
 export const TESTIMONIAL_API_SERVICE = '/api/testimonials?populate[0]=media_url&populate[2]=icon&populate[3]=profile_picture&filters[service_type][$eq]=<service_type>&pagination[page]=<pageNo>&pagination[pageSize]=<pageSize>';
 export const COACHING_API ='/api/coaching-pages?populate[0]=media_url&populate[2]=footers.media_url&populate[3]=coaching_page_contents.media_url&populate[4]=coaching_page_contents.coaching_services.media_url&populate[5]=coaching_page_contents.coaching_services.coaching_service_contents&populate[6]=coaching_page_contents';
 export const TESTIMONIAL_COACHING_API = '/api/testimonials?populate[0]=media_url&populate[2]=icon&populate[3]=profile_picture&filters[service_type][$contains]=coaching';
-export const NEWS_ALERT_DATA_API = '/api/blogs/?populate[0]=media_url&populate[1]=author_profile_image.media_url&populate[2]=sub_service&pagination[page]=1&pagination[pageSize]=8&filters[updatedAt][$gte]=<startDate>&filters[toBeFeatured][$eq]=true';
+export const NEWS_ALERT_DATA_API = '/api/blogs/?populate[0]=media_url&populate[1]=author_profile_image.media_url&populate[2]=sub_service&filters[updatedAt][$gte]=<startDate>&filters[toBeFeatured][$eq]=true';
 
 export const getNewsAlertDataApi = () => {
   const currentdate = new Date();
