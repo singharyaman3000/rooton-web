ca683ba5d1592062a5439ae7cab4a57247313cb4
mithunnqb <mithunn@qburst.com>
2023-12-21 15:20:46 +0530

    Update AfterInstall.sh

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -3,5 +3,5 @@ set -e
 source /home/deploy/.profile
 /usr/bin/sudo /bin/chown -R deploy.deploy /home/deploy/codedeploy/fe/v1/
 ##cd /home/deploy/codedeploy/fe/v1/
-#rm -rf node_modules
+##rm -rf node_modules
 #npm install
