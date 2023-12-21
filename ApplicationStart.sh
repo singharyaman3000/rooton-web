7cb4c503da1bd876808f2e6b8bbfdaef5ce20495
mithunnqb <mithunn@qburst.com>
2023-12-21 16:16:04 +0530

    Update ApplicationStart.sh

--- ApplicationStart.sh
+++ ApplicationStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
 source /home/deploy/.profile
-##pm2 delete rooton-fe-codedeploy
-##pm2 start npm --name "rooton-fe-codedeploy" -- start
+cd /home/deploy/codedeploy/fe/v1/
+pm2 delete rooton-fe-codedeploy
+pm2 start npm --name "rooton-fe-codedeploy" -- start
