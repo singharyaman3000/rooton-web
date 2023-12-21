5a4f7e7787c3e439e5bb8667e96558a1f9361dfd
mithunnqb <mithunn@qburst.com>
2023-12-21 15:38:57 +0530

    Update AfterInstall.sh

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -4,4 +4,4 @@ source /home/deploy/.profile
 /usr/bin/sudo /bin/chown -R deploy.deploy /home/deploy/codedeploy/fe/v1/
 cd /home/deploy/codedeploy/fe/v1/
 rm -rf node_modules
-npm install
+npm install --legacy-peer-deps
