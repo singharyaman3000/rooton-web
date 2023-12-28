5f7602155977ef6490cc41f08c26a287aceda728
Harish Jinagam <138629649+jharish91@users.noreply.github.com>
2023-12-27 20:59:08 -0400

    Tracking Click Events

--- gtag.js
+++ gtag.js
@@ -6,10 +6,9 @@ export const pageview = (url) => {
   });
 };
 
-export const event = ({ action, category, label, value }) => {
+export const trackEvent = ({ action, category, label }) => {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
-    value,
   });
 };
