efe17d1f820469a847d05c3eaa518f7599d115c9
Aryaman Singh <aryaman.singh@rooton.ca>
2024-03-19 17:16:09 +0530

    Added FacebookPixel & Microsoft Clarity
    Updated Folder Structure for Analytics...
    Added FacebookPixel & Microsoft Clarity
    
    Added Two more .env variables

---
+++ src/app/metrics/index.tsx
@@ -0,0 +1,13 @@
+import GoogleAnalytics from './GoogleAnalytics';
+import MicrosoftClarity from './MicrosoftClarity';
+import FacebookPixel from './FacebookPixel';
+
+const Metrics = () => {return (
+  <>
+    <GoogleAnalytics />
+    <MicrosoftClarity />
+    <FacebookPixel />
+  </>
+);};
+
+export default Metrics;
