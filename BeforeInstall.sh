06abd7c5cf1de8dd643595a22f8d04b6b544d4ca
aryamanatrooton <122260110+aryamanatrooton@users.noreply.github.com>
2024-03-23 21:39:50 +0530

    Update BeforeInstall.sh

--- BeforeInstall.sh
+++ BeforeInstall.sh
@@ -2,5 +2,5 @@
 set -e
 source /home/deploy/.profile
 rm -rf /home/deploy/codedeploy/fe/v1/*
-cp /home/deploy/codedeploy/fe/.env /home/deploy/codedeploy/fe/v1/
+sudo cp /home/deploy/codedeploy/fe/.env /home/deploy/codedeploy/fe/v1/
 pm2 ls
