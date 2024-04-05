56af3cdfc353973bd3b9c50c778531e52af8ca4b
aryamanatrooton <aryaman.singh@rooton.ca>
2024-04-06 02:53:54 +0530

    Update FacebookPixel.jsx

--- src/app/metrics/FacebookPixel.jsx
+++ src/app/metrics/FacebookPixel.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+
 'use client';
 
 import { usePathname } from 'next/navigation';
@@ -10,20 +13,40 @@ const FacebookPixel = () => {
   const pathname = usePathname();
 
   useEffect(() => {
-    if (!loaded) return;
-
-    pixel.pageview();
+    // Once the script is loaded and the pathname changes, trigger a page view
+    if (loaded) {
+      pixel.pageview();
+    }
   }, [pathname, loaded]);
 
   return (
     <div>
       <Script
-        id="fb-pixel"
-        src="/scripts/pixel.js"
+        id="fb-pixel-script"
         strategy="afterInteractive"
         onLoad={() => {return setLoaded(true);}}
-        data-pixel-id={pixel.FB_PIXEL_ID}
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src='https://connect.facebook.net/en_US/fbevents.js';
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s);}(window, document, 'script');
+            fbq('init', '${pixel.FB_PIXEL_ID}');
+          `,
+        }}
       />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${pixel.FB_PIXEL_ID}&ev=PageView&noscript=1`}
+        />
+      </noscript>
     </div>
   );
 };
