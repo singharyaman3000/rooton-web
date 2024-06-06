3b87ba1785dcc6fa9d679c2b08001497fdb75946
Aryaman Singh <aryaman.singh@rooton.ca>
2024-06-06 18:53:13 +0530

    chore: updated API Payload

--- src/utils/docuFetch.tsx
+++ src/utils/docuFetch.tsx
@@ -10,7 +10,7 @@ const getUserDoc = async (): Promise<string | undefined> => {
       return undefined;
     }
 
-    const response = await axios.post(`${process.env.NEXT_SERVER_API_BASE_URL}/api/userDoc`, 'harish.jinagam@rooton.ca-sv' , {
+    const response = await axios.post(`${process.env.NEXT_SERVER_API_BASE_URL}/api/userDoc`, { email:'harish.jinagam@rooton.ca', serveDoc:'sp' } , {
       headers: { Authorization: `Bearer ${token}` },
     });
     return response.data.Slug;
