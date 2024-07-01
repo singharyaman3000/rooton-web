db5be3bb1505b10c7275d4dde03f56b3425ba019
aryamanatrooton <122260110+aryamanatrooton@users.noreply.github.com>
2024-07-01 23:39:17 +0530

    Update BeforeInstall.sh

--- BeforeInstall.sh
+++ BeforeInstall.sh
@@ -2,6 +2,6 @@
 set -e
 source /home/deploy/.profile
 pm2 stop rooton-fe-codedeploy
-sudo rm -rf /home/deploy/codedeploy/fe/v1/*
+sudo rm -rf /home/deploy/codedeploy/fe/v1/* /home/deploy/codedeploy/fe/v1/.[!.]* /home/deploy/codedeploy/fe/v1/..?*
 sudo cp /home/deploy/codedeploy/fe/.env /home/deploy/codedeploy/fe/v1/
 pm2 ls
