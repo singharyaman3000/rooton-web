fdda78a6d4945443f9e9587ac12ed61d323c83d7
jharish91 <138629649+jharish91@users.noreply.github.com>
2024-08-14 21:57:45 -0300

    Update ChatInterface.tsx

--- src/components/ToolsPage-Services/RAG-Chatbot/ChatInterface.tsx
+++ src/components/ToolsPage-Services/RAG-Chatbot/ChatInterface.tsx
@@ -143,7 +143,7 @@ const ChatInterface = ({ resetChat, setResetChat }: IChatInterfaceProps) => {
                       variant="outlined"
                       sx={{
                         display: 'inline-block',
-                        p: 1,
+                        p: 3,
                         border: '0',
                         boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px',
                         bgcolor: chat.speaker === 'human' ? '#ffc766' : 'white',
